2d8d6762e5dec7c3238046be70930224c7564a56
XINKUN LIN <xinkun.lin124@gmail.com>
2025-04-25 01:11:38 +1000

    Delete Swin-Transformer directory

--- Swin-Transformer/data/__init__.py
+++
@@ -1,7 +0,0 @@
-from .build import build_loader as _build_loader
-from .losses import FocalCrossEntropyCombo as focal_ce
-
-def build_loader(config, simmim=False, is_pretrain=False):
-    if not simmim:
-        return _build_loader(config)
-
